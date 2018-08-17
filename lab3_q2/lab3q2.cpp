f86f6882f642b0749b2c211674c114fc4bd1b949
rasmiranjansamantray <42238972+rasmiranjansamantray@users.noreply.github.com>
2018-08-17 11:02:47 +0530

    Add files via upload

---
+++ lab3_q2/lab3q2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+     int a=10;                   //initial value = 10
+     int b(5);                   //initial value = 5
+     int result;                 //initial value
+//undetermined
+
+   a = a + 7;
+   result = a - b;
+   cout<<result;
+    
+
+    return 0;
+}
